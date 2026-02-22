b020581fb46c91ac78b03cc09b5b1814176dd915
dora@dora.me <dora@dora.me>
2026-02-22 07:22:24 +0000

    TDSE

---
+++ Quantum_Tunneling/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+// Here we will try to resolve the Time dependent Schro equation: 
+// --------> Initialize a Gaussian wave packet representing a particle.
+// --------> Evolve it in time as it approaches a rectangular potential barrier.
+// --------> We need to observe the wave packet splitting: part of it reflects, and part of it "tunnels" through.
+
+int main(void)
+{
+
+  return 0;
+}
